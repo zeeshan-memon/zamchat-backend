c115dfa7c8dc7da70ddb648cc1c4c66a2cdea47a
zeeshan-memon <52953436+zeeshan-memon@users.noreply.github.com>
2021-09-21 10:44:53 +0500

    Update accounts.js

--- models/accounts.js
+++ models/accounts.js
@@ -1,9 +1,103 @@
-const mongoose = require("mongoose");
+module.exports.model = (db, Sequelize) => {
+    let model = {
+        id: {
+            type: Sequelize.BIGINT,
+            unique: true,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        firstName: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        lastName: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        email: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        city: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        state: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        zip: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        address: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        address2: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        contact: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null,
+        },
+        image: {
+            type: Sequelize.STRING(5000),
+            defaultValueValue: null
+        },
+        token: {
+            type: Sequelize.STRING(5000),
+            defaultValueValue: null
+        },
+        password: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: null
+        },
+        role: {
+            type: Sequelize.STRING(500),
+            defaultValueValue: 'user',
+            values: ['user', 'admin' , 'anonymous']
+        },
 
- const users= new mongoose.Schema({
-    name:{
-        type: String
+        accountType: {
+            type: Sequelize.STRING(500),
+            defaultValue: 'normal',
+            values: ['normal', 'social']
+        },
+        area: {
+            type: Sequelize.STRING(500),
+            defaultValue: null
+        },
+        deletedBy: {
+            type: Sequelize.STRING(500),
+            defaultValue: null
+        },
+        deleteDone: {
+            type: Sequelize.DATE,
+            defaultValue: null
+        },
+        isVarified: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false
+        },
+        isDeleted: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false,
+            values: [true, false]
+        },
+        createDate: {
+            type: Sequelize.DATE,
+            defaultValue: new Date()
+        },
+        updatedDate: {
+            type: Sequelize.DATE,
+            defaultValue: new Date()
+        },
     }
-})
-var User = mongoose.model('user', users);
-module.exports = User
+
+    return db.define('accounts', model, {
+        db
+    });
+}
